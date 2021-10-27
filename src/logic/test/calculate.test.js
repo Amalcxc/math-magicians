a8adbfe9ac2aa3223a67f93d4fbbe1dc4984c1e0
Amalcxc <amalcxirsi@gmail.com>
2021-10-28 00:59:05 +0300

    negative tests

--- src/logic/test/calculate.test.js
+++ src/logic/test/calculate.test.js
@@ -48,6 +48,7 @@ describe('Testing 5 + 10 = 15', () => {
     const res = calculate(emptyCalc, '5');
     expect(res).toStrictEqual(oneNumberPressed);
   });
+
   test('Pressing + after pressing 5', () => {
     const res = calculate(oneNumberPressed, '+');
     expect(res).toStrictEqual(operationSymbolPressed);
@@ -74,5 +75,45 @@ describe('Testing 5.5', () => {
   test('Pressing 5 after .', () => {
     const res = calculate(dotPressed, '5');
     expect(res).toStrictEqual(numberAfterDot);
+  });
+});
+
+describe('negative Testing 5.5', () => {
+  test('Pressing 5 as the first number', () => {
+    const res = calculate(emptyCalc, '5');
+    expect(res).not.toStrictEqual(emptyCalc);
+  });
+  test('Pressing . after 5', () => {
+    const res = calculate(oneNumberPressed, '.');
+    expect(res).not.toStrictEqual(oneNumberPressed);
+  });
+  test('Pressing 5 after .', () => {
+    const res = calculate(dotPressed, '5');
+    expect(res).not.toStrictEqual(dotPressed);
+  });
+});
+
+
+describe('negative Testing 5 + 10 = 15', () => {
+  test('Pressing AC for the first time', () => {
+    const res = calculate(emptyCalc, 'AC');
+    expect(res).not.toStrictEqual(oneNumberPressed);
+  });
+  test('Pressing 5 as the first number', () => {
+    const res = calculate(emptyCalc, '5');
+    expect(res).not.toStrictEqual(emptyCalc);
+  });
+
+  test('Pressing + after pressing 5', () => {
+    const res = calculate(oneNumberPressed, '+');
+    expect(res).not.toStrictEqual(oneNumberPressed);
+  });
+  test('Pressing 10 after pressing +', () => {
+    const res = calculate(operationSymbolPressed, '10');
+    expect(res).not.toStrictEqual('1');
+  });
+  test('Pressing = after pressing 10', () => {
+    const res = calculate(numberAfterOperation, '=');
+    expect(res).not.toStrictEqual('0');
   });
 });
--- src/logic/test/operate.test.js
+++ src/logic/test/operate.test.js
@@ -17,3 +17,22 @@ describe('testing the operations', () => {
     expect(operate(10, 2, '÷')).toBe('5');
   });
 });
+
+
+describe('negative testing the operations', () => {
+  test('adds 1 + 2 not to equal 5', () => {
+    expect(operate(1, 2, '+')).not.toBe('5');
+  });
+
+  test('minus 1 - 2 not to equal 7', () => {
+    expect(operate(1, 2, '-')).not.toBe('7');
+  });
+
+  test('times 0 x 2 not to equal -6', () => {
+    expect(operate(0, 2, 'x')).not.toBe('-6');
+  });
+
+  test('division 10 ÷ 2 not to equal 12', () => {
+    expect(operate(10, 2, '÷')).not.toBe('12');
+  });
+});
