b3fef37a9a67b6eb95e26d43cbb239769a3ec016
Amalcxc <amalcxirsi@gmail.com>
2021-10-27 22:43:25 +0300

    operate.js tests add in the file logic

---
+++ src/logic/test/operate.test.js
@@ -0,0 +1,22 @@
+import operate from '../operate'
+
+describe( 'testing the operations', () => {
+  
+  test('adds 1 + 2 to equal 3', () => {
+    expect(operate(1, 2, '+')).toBe('3');
+  });
+
+  test('minus 1 - 2 to equal -1', () => {
+    expect(operate(1, 2, '-')).toBe('-1');
+  });
+
+  test('times 0 x 2 to equal 0', () => {
+    expect(operate(0, 2, 'x')).toBe('0');
+  });
+
+  test('division 10 ÷ 2 to equal 5', () => {
+    expect(operate(10, 2, '÷')).toBe('5');
+  });
+}
+)
+
